d75cf34419d51312626f0f7d7fc56b5f6cae4350
zhaolong <928343994@qq.com>
2020-12-08 20:49:25 +0800

    第四讲 动态权限申请

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ Flutter 脚手架 快速开发模版
 ## Getting Started
 
 *  2020 -12-05  第一天 [Flutter应用开发从0到1基础架构搭建效果预览](https://www.bilibili.com/video/BV1rZ4y1G7cK/)
-*  2020 -12-06  第二天[Flutter项目创建、Android Studio的基本断点调试 应用图标更换、应用名称修改](https://www.bilibili.com/video/BV1pt4y1Y7F8/)
-*  2020 -12-07  第三天[Flutter从0到1构建应用启动页面、闪屏页面 600行字幕 带大家轻松玩转-正在发布]()
+*  2020 -12-06  第二天 [Flutter项目创建、Android Studio的基本断点调试 应用图标更换、应用名称修改](https://www.bilibili.com/video/BV1pt4y1Y7F8/)
+*  2020 -12-07  第三天 [Flutter从0到1构建应用启动页面、闪屏页面 600行字幕 带大家轻松玩转-正在发布](https://www.bilibili.com/video/BV1VA41147WG/)
 
--- lib/src/index_page.dart
+++ lib/src/index_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 /// 创建人： Created by zhaolong
 /// 创建时间：Created by  on 2020/12/7.
@@ -7,7 +9,7 @@ import 'package:flutter/material.dart';
 /// 可关注公众号：我的大前端生涯   获取最新技术分享
 /// 可关注网易云课堂：https://study.163.com/instructor/1021406098.htm
 /// 可关注博客：https://blog.csdn.net/zl18603543572
-///
+/// 启动默认显示的页面
 /// 代码清单
 
 class IndexPage extends StatefulWidget {
@@ -17,7 +19,34 @@ class IndexPage extends StatefulWidget {
   }
 }
 
-class _IndexPageState extends State {
+class _IndexPageState extends State with WidgetsBindingObserver {
+  //页面的初始化函数
+  @override
+  void initState() {
+    super.initState();
+
+    checkPermisson();
+
+    //注册观察者
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed && _isGoSetting) {
+      checkPermisson();
+    }
+  }
+
+  @override
+  void dispose() {
+    //注销观察者
+    WidgetsBinding.instance.removeObserver(this);
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,5 +58,92 @@ class _IndexPageState extends State {
         ),
       ),
     );
+  }
+
+  void checkPermisson({PermissionStatus status}) async {
+    //权限
+    Permission permission = Permission.storage;
+
+    if (status == null) {
+      //权限状态
+      status = await permission.status;
+    }
+
+    if (status.isUndetermined) {
+      //第一次申请
+      showPermissonAlert(_list[0], "同意", permission);
+    } else if (status.isDenied) {
+      //用户第一次申请拒绝
+      showPermissonAlert(_list[1], "重试", permission);
+    } else if (status.isPermanentlyDenied) {
+      //第二次申请 用户拒绝
+      showPermissonAlert(_list[2], "去设置中心", permission, isSetting: true);
+    } else {
+      //通过
+    }
+  }
+
+  List<String> _list = [
+    "为您更好的体验应用，所以需要获取您的手机文件存储权限，以保存您的一些偏好设置",
+    "您已拒绝权限，所以无法保存您的一些偏好设置，将无法使用APP",
+    "您已拒绝权限，请在设置中心中同意APP的权限请求",
+    "其他错误"
+  ];
+
+  //是否去设置中心
+  bool _isGoSetting = false;
+
+  void showPermissonAlert(
+      String message, String rightString, Permission permission,
+      {bool isSetting = false}) {
+    showCupertinoDialog(
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text("温馨提示"),
+            content: Container(
+              padding: EdgeInsets.all(12),
+              child: Text(message),
+            ),
+            actions: [
+              //左边的按钮
+              CupertinoDialogAction(
+                child: Text("退出应用"),
+                onPressed: () {
+                  closeApp();
+                },
+              ),
+              //右边的按钮
+              CupertinoDialogAction(
+                child: Text("$rightString"),
+                onPressed: () {
+                  //关闭弹框
+                  Navigator.of(context).pop();
+                  if (isSetting) {
+                    _isGoSetting = true;
+                    //去设置中心
+                    openAppSettings();
+                  } else {
+                    //申请权限
+                    requestPermiss(permission);
+                  }
+                },
+              )
+            ],
+          );
+        },
+        context: context);
+  }
+
+  void requestPermiss(Permission permission) async {
+    //发起权限申请
+    PermissionStatus status = await permission.request();
+
+    //校验
+    checkPermisson();
+  }
+
+  void closeApp() {
+    //关闭应用的方法
+    SystemChannels.platform.invokeMethod("SystemNavigator.pop");
   }
 }
